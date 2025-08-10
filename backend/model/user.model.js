27d9fa998e3817d6e075841ead9e7316ccece225
Pongsakorn Thongrak <143980452+decemberlnwza007@users.noreply.github.com>
2025-08-10 13:23:34 +0700

    Revision 08

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -8,6 +8,14 @@ const getAllUsers = async () => {
     return result.rows;
 }
 
+const createUser = async (username, email, password, firstname, lastname, image) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await db.query(
+        'INSERT INTO users (username, email, password, firstname, lastname, image) VALUES ($1, $2, $3, $4'
+    )
+}
+
 module.exports = {
     getAllUsers,
 }
